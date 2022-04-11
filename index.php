6953c955a9716fde86fd771ec361968f0df24391
OrbienVincentRay <geometry172@gmail.com>
2022-04-12 03:39:37 +0800

    song1 added

--- index.php
+++ index.php
@@ -14,11 +14,11 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> ChocoMingBeats </a>
+                <a class="navbar-brand" href="http://localhost/melomaniac-activity/index.php"> ChocoMingBeats </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="http://localhost/melomaniac-activity/song1.php">Song 1</a></li>
                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
@@ -44,7 +44,117 @@
                     <div class="col-lg-12 text-center">
                         <h2 class="text-primary">Touch By Touch</h2>
                         <p class="lead">Joy</p>
-                        <p>display the lyrics/chords here</p>
+                        <?php
+                            echo "<p><h6>Tuning: E A D G B E</h6></p>";
+                        ?>
+                        <div class="col-lg-6 mb-2 pr-lg-1"><img src="Images/guitarchordstouch.png" width="300" height="110"> </div>
+
+
+                        <?php
+                        echo "<p><h6>Cm         Fm    </h6></p>";
+                        echo "<p> Touch by touch </p>";
+                        echo "<p><h6>Bb                 Eb</h6></p>";
+                        echo "<p> You're my all time lover </p>";
+                        echo "<p><h6>Cm         Fm    </h6></p>";
+                        echo "<p> Skin to skin </p>";
+                        echo "<p><h6>Bb                 Eb</h6></p>";
+                        echo "<p>Come under my cover </p>";
+                        echo "<br/>";
+                        echo "<p><h6>[Solo 1]</h6></p>";
+                        echo "<p><h6>Cm Eb Bb Cm</h6></p>";
+                        echo "<br/>";
+                        echo "<p><h6>[Verse 1]</h6></p>";
+                        echo "<p><h6>Cm</h6></p>";
+                        echo "<p>When I feel the time is right </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>And you're staying by my side </p>";
+                        echo "<p><h6>Bb</h6></p>";
+                        echo "<p>And the love you give to me </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>Makes my heart beat </p>";
+                        echo "<br/>";
+                        echo "<p><h6>Cm</h6></p>";
+                        echo "<p>When my fantasies fly away </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>But my feelings make me stay </p>";
+                        echo "<p><h6>Bb</h6></p>";
+                        echo "<p>I see the fire in your eyes </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>It makes my heart beat </p>";
+                        echo "<br/>";
+                        echo "<p><h6>[Chorus]</h6></p>";
+                        echo "<p><h6>Cm</h6></p>";
+                        echo "<p>Do it, we still do it </p>";
+                        echo "<p><h6>Fm</h6></p>";
+                        echo "<p>Night and day</p>";
+                        echo "<p><h6>Bb                 Eb</h6></p>";
+                        echo "<p>You're my all time lover</p>";
+                        echo "<p><h6>Cm</h6></p>";
+                        echo "<p>Do it, we still do it</p>";
+                        echo "<p><h6>Fm</h6></p>";
+                        echo "<p>In a Way</p>";
+                        echo "<p><h6>Bb                 Eb</h6></p>";
+                        echo "<p>Like there is no other</p>";
+                        echo "<p><h6>Cm         Fm    </h6></p>";
+                        echo "<p>Touch by touch</p>";
+                        echo "<p><h6>Bb                 Eb</h6></p>";
+                        echo "<p>You're my all time lover</p>";
+                        echo "<p><h6>Cm         Fm    </h6></p>";  
+                        echo "<p>Skin to skin</p>";
+                        echo "<p><h6>Bb                 Eb</h6></p>";
+                        echo "<p>Come under my cover </p>";
+                        echo "<br/>";
+                        echo "<p><h6>[Solo 2]</h6></p>";
+                        echo "<p><h6>Cm Eb Bb Cm</h6></p>";
+                        echo "<br/>";
+                        echo "<p><h6>[Verse 2]</h6></p>";
+                        echo "<p><h6>Cm</h6></p>";
+                        echo "<p>When my heart is full of love </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>It makes me a total dove </p>";
+                        echo "<p><h6>Bb</h6></p>";
+                        echo "<p>Love's the game that we play </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>Night and day</p>";
+                        echo "<p><h6>Cm</h6></p>";
+                        echo "<p>When our love is clear and bright </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>It's our way to see the light </p>";
+                        echo "<p><h6>Bb</h6></p>";
+                        echo "<p>There's a fire in our hearts </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>Night and day</p>";
+                        echo "<br/>";
+                        echo "<p><h6>[Repeat Chorus]</h6></p>";
+                        echo "<br/>";
+                        echo "<p><h6>[Solo 3]</h6></p>";
+                        echo "<p><h6>[Repeat Chorus]</h6></p>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                
+
+
+                        ?>
                     </div>
                 </div>
             </div>
---
+++ song1.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="http://localhost/melomaniac-activity/index.php"> ChocoMingBeats</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to ChocoMingBeats</h1>
+                <p class="lead">Kahit kailan hindi magiging sapat ang never enough uwu </p>
+                <p class="lead">ChocoMingBeats presents you with the greatest all time hits of the year! </p>
+                <p class="lead">You can get up to 1 month of free trial if you refer us to your friends! </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-primary">Touch By Touch</h2>
+                        <p class="lead">Joy</p>
+                        <?php
+                            echo "<p><h6>Tuning: E A D G B E</h6></p>";
+                        ?>
+                        <div class="col-lg-6 mb-2 pr-lg-1"><img src="Images/guitarchordstouch.png" width="300" height="110"> </div>
+
+
+                        <?php
+                        echo "<p><h6>Cm         Fm    </h6></p>";
+                        echo "<p> Touch by touch </p>";
+                        echo "<p><h6>Bb                 Eb</h6></p>";
+                        echo "<p> You're my all time lover </p>";
+                        echo "<p><h6>Cm         Fm    </h6></p>";
+                        echo "<p> Skin to skin </p>";
+                        echo "<p><h6>Bb                 Eb</h6></p>";
+                        echo "<p>Come under my cover </p>";
+                        echo "<br/>";
+                        echo "<p><h6>[Solo 1]</h6></p>";
+                        echo "<p><h6>Cm Eb Bb Cm</h6></p>";
+                        echo "<br/>";
+                        echo "<p><h6>[Verse 1]</h6></p>";
+                        echo "<p><h6>Cm</h6></p>";
+                        echo "<p>When I feel the time is right </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>And you're staying by my side </p>";
+                        echo "<p><h6>Bb</h6></p>";
+                        echo "<p>And the love you give to me </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>Makes my heart beat </p>";
+                        echo "<br/>";
+                        echo "<p><h6>Cm</h6></p>";
+                        echo "<p>When my fantasies fly away </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>But my feelings make me stay </p>";
+                        echo "<p><h6>Bb</h6></p>";
+                        echo "<p>I see the fire in your eyes </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>It makes my heart beat </p>";
+                        echo "<br/>";
+                        echo "<p><h6>[Chorus]</h6></p>";
+                        echo "<p><h6>Cm</h6></p>";
+                        echo "<p>Do it, we still do it </p>";
+                        echo "<p><h6>Fm</h6></p>";
+                        echo "<p>Night and day</p>";
+                        echo "<p><h6>Bb                 Eb</h6></p>";
+                        echo "<p>You're my all time lover</p>";
+                        echo "<p><h6>Cm</h6></p>";
+                        echo "<p>Do it, we still do it</p>";
+                        echo "<p><h6>Fm</h6></p>";
+                        echo "<p>In a Way</p>";
+                        echo "<p><h6>Bb                 Eb</h6></p>";
+                        echo "<p>Like there is no other</p>";
+                        echo "<p><h6>Cm         Fm    </h6></p>";
+                        echo "<p>Touch by touch</p>";
+                        echo "<p><h6>Bb                 Eb</h6></p>";
+                        echo "<p>You're my all time lover</p>";
+                        echo "<p><h6>Cm         Fm    </h6></p>";  
+                        echo "<p>Skin to skin</p>";
+                        echo "<p><h6>Bb                 Eb</h6></p>";
+                        echo "<p>Come under my cover </p>";
+                        echo "<br/>";
+                        echo "<p><h6>[Solo 2]</h6></p>";
+                        echo "<p><h6>Cm Eb Bb Cm</h6></p>";
+                        echo "<br/>";
+                        echo "<p><h6>[Verse 2]</h6></p>";
+                        echo "<p><h6>Cm</h6></p>";
+                        echo "<p>When my heart is full of love </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>It makes me a total dove </p>";
+                        echo "<p><h6>Bb</h6></p>";
+                        echo "<p>Love's the game that we play </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>Night and day</p>";
+                        echo "<p><h6>Cm</h6></p>";
+                        echo "<p>When our love is clear and bright </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>It's our way to see the light </p>";
+                        echo "<p><h6>Bb</h6></p>";
+                        echo "<p>There's a fire in our hearts </p>";
+                        echo "<p><h6>Eb</h6></p>";
+                        echo "<p>Night and day</p>";
+                        echo "<br/>";
+                        echo "<p><h6>[Repeat Chorus]</h6></p>";
+                        echo "<br/>";
+                        echo "<p><h6>[Solo 3]</h6></p>";
+                        echo "<p><h6>[Repeat Chorus]</h6></p>";
+
+
+
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. add your fullname here</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
